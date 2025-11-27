c1eb0848ef05a990eafc01b09cb9cc887a1adef6
Joseph Abdullaahi <josephabdullaahi@gmail.com>
2025-11-27 09:54:37 -0800

    feat: Added a background graident

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
 
 function App() {
   return (
-    <div>App</div>
-  )
+    <div className="min-h-screen w-full relative">
+      <div
+        className="absolute inset-0 z-0"
+        style={{
+          background:
+            "radial-gradient(125% 125% at 50% 100%, #000000 40%, #2b0707 100%)",
+        }}
+      />
+    </div>
+  );
 }
 
-export default App
+export default App;
